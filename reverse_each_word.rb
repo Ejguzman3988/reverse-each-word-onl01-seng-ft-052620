914c5f0d48ed39e7f8f9982ecf5034497109a2ae
Eriberto Guzman <eriberto176@gmail.com>
2020-05-29 22:59:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
-  reverse_sentence = []
+# def reverse_each_word(sentence)
+#   new_sentence = sentence.split(" ")
+#   reverse_sentence = []
   
-  new_sentence.each do |word|
+#   new_sentence.each do |word|
     
-    reverse_sentence << word.reverse
-  end
-  reverse_sentence.join(' ')
+#     reverse_sentence << word.reverse
+#   end
+#   reverse_sentence.join(' ')
   
-end
+# end
